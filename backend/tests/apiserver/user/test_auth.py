4460a0b1e91c2239d26d15bbec35a00b057ba13a
Maik Rebaum <maik.rebaum@canonical.com>
2026-04-17 12:00:54 -0400

    test: add profiles PATCH to auth test

--- backend/tests/apiserver/user/test_auth.py
+++ backend/tests/apiserver/user/test_auth.py
@@ -69,6 +69,7 @@ AUTHENTICATED_ROUTES = (
     ("GET", "/api/v1/profiles/{id}"),
     ("POST", "/api/v1/profiles"),
     ("DELETE", "/api/v1/profiles/{id}"),
+    ("PATCH", "/api/v1/profiles/{id}"),
 )
 
 UNAUTHENTICATED_ROUTES = (("POST", "/api/v1/login"),)
