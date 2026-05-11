e1416146001f9e66ec877c8ee176d8b4e6f9262c
Wyatt Rees <wyatt.rees@canonical.com>
2026-05-11 14:59:11 +0200

    Bad lint

--- backend/msm/apiserver/site/handlers/images.py
+++ backend/msm/apiserver/site/handlers/images.py
@@ -71,10 +71,7 @@ class S3StreamResponse(StreamingResponse):
             chunk = await run_in_threadpool(body.read, self.chunk_size)
             if not chunk:
                 break
-            await send(
-                {
-                    "type": "http.response.body",
-                    "body": chunk,
+            await send({"type": "http.response.body", "body": chunk,
                     "more_body": True,
                 }
             )
