90495e9c5582ae67ca3caa615a3888e72d2d015d
Ahmet Can Buyukyilmaz <ahmet.buyukyilmaz@canonical.com>
2026-06-25 13:42:12 +0300

    refactor(fe-settings): migrated ImageSourceListTable to GenericTable

--- frontend/src/app/settings/views/ImageSourceList/ImageSourceListTable/ImageSourceListTable.tsx
+++ frontend/src/app/settings/views/ImageSourceList/ImageSourceListTable/ImageSourceListTable.tsx
@@ -1,59 +1,37 @@
-import { DynamicTable } from "@canonical/maas-react-components";
-import { flexRender, getCoreRowModel, useReactTable } from "@tanstack/react-table";
+import { GenericTable } from "@canonical/maas-react-components";
 
 import { useImageSourceTableColumns } from "../hooks";
 
 import type { BootSource } from "@/app/apiclient";
 import TableCaption from "@/app/base/components/TableCaption";
 
-type Props = {
+type ImageSourcesListTableProps = {
   data: BootSource[];
   error: Error | null;
   isPending: boolean;
 };
 
-const ImageSourceListTable = ({ data, error, isPending }: Props) => {
+const ImageSourceListTable = ({ data, error, isPending }: ImageSourcesListTableProps) => {
   const columns = useImageSourceTableColumns();
-  const imageSourceTable = useReactTable<BootSource>({
-    data: data,
-    columns,
-    getCoreRowModel: getCoreRowModel(),
-    enableSorting: false,
-  });
 
   return (
-    <DynamicTable aria-label="Image source list" className="image-source-table" variant="regular">
-      <thead>
-        {imageSourceTable.getHeaderGroups().map((headerGroup) => (
-          <tr key={headerGroup.id}>
-            {headerGroup.headers.map((header) => (
-              <th className={header.column.id} colSpan={header.colSpan} key={header.id}>
-                {flexRender(header.column.columnDef.header, header.getContext())}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      {error ? (
-        <TableCaption inTable>
-          <TableCaption.Error error={error} />
-        </TableCaption>
-      ) : isPending ? (
-        <DynamicTable.Loading table={imageSourceTable} />
-      ) : (
-        <DynamicTable.Body>
-          {imageSourceTable.getRowModel().rows.map((row) => (
-            <tr key={row.id}>
-              {row.getVisibleCells().map((cell) => (
-                <td className={cell.column.id} key={cell.id}>
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </DynamicTable.Body>
-      )}
-    </DynamicTable>
+    <GenericTable
+      aria-label="Image source list"
+      className="image-source-table"
+      columns={columns}
+      data={data}
+      isLoading={isPending}
+      loadingVariant="skeleton"
+      noData={
+        error ? (
+          <TableCaption>
+            <TableCaption.Error error={error} />
+          </TableCaption>
+        ) : (
+          "No image sources configured."
+        )
+      }
+    />
   );
 };
 
